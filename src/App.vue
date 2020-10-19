4fabce35ca7eaec39311279570d5051c9cd3c901
Sadaqat Panhwer <panhwersadaqat22@gmail.com>
2020-10-19 23:59:09 +0500

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="container">
+    <appHeader></appHeader>
+    <div class="row">
+      <div class="col-md-12">
+        <router-view></router-view>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script> 
+import Header from "./components/common/Header";
+
+export default {
+  components: {
+    appHeader: Header,
+  },
+  created() {
+    this.$store.dispatch('initStocks');
+  }
+};
+</script>
+
+<style>
+body {
+  padding: 30px;
+  box-sizing: border-box;
+ background-color:rgb(223, 219, 219);
+
+}
+</style>
---
+++ src/components/stocks/Stocks.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="row">
+    <appStock v-for="stock in stocks" :key="stock.id" :stock="stock"></appStock>
+
+    <!-- <router-view></router-view> -->
+  </div>
+</template>
+
+<script>
+import Stock from "./Stock";
+
+export default {
+  components: {
+    appStock: Stock,
+  },
+  computed:{
+    stocks(){
+      return this.$store.getters.stocks;
+    }
+  }
+};
+</script>
+
+<style></style>
